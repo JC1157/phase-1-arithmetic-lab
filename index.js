4834310ef32dc9a8f37c1891ce4dfc15fed4abe2
Juan Baez <juan.baez2093@gmail.com>
2022-10-07 22:46:52 -0400

    commit tree f138e6a27e8a08ed5f69c1f6b8b6f99c77eb904a

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 31; 
+const num2= 2; 
+const multiply= num1 * num2; 
+
+// generates a random integer greater than 0
+
+const random= Math.floor(Math.random() * 10) + 1;
+
+// is an equation that calculates a remainder that is equal to 4
+const num3= 4;
+const num4= 1; 
+const mod= num3 / num4;  
+
+// will return 20 as the highest number in the set
+
+const max= Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20); 
